e5b320895f01fded28a9ba406e1895fede4b5ce0
Kim In Seop <opnayn@gmail.com>
2020-12-23 19:11:34 +0900

    🚑 Fix: Don't export types in src/index

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 /// <reference path="../types/shaka-player.d.ts" />
 
-export { default as ShakaVideo, ShakaVideoProps } from './ShakaVideo';
+export { default as ShakaVideo } from './ShakaVideo';
 export * from './utils/shaka';
